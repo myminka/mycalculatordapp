7d7bc7c90cae7f6466fe1d358f0f3d297bbd1422
Maksim Silkou <maxim.silkou.2002@gmail.com>
2024-06-08 15:42:20 +0200

    update tests

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -5,9 +5,25 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod mycalculatordapp {
     use super::*;
-    pub fn create(ctx: Context<Create>, init_message: String) => ProgramResult{
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
-        Ok();
+        return Ok(());
     }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Calculator{
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64
 }
